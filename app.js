33194aeb88eae08aa7e79213e366766882fd04bd
Truongardinho <120437680+Truongardinho@users.noreply.github.com>
2024-04-05 19:51:14 +0700

    app.js

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const html = `
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <h1>Nguyen Manh Truong</h1>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
